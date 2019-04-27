791dcd90b61763fc93a42a32e08ef739c1b48727
Sunkian <alice.pagnoux@gmail.com>
2019-04-27 15:04:42 +0200

    Add coments

--- app/models/rating.js
+++ app/models/rating.js
@@ -13,22 +13,20 @@ const ratingSchema_ = new mongoose.Schema({
 const Rating_ = mongoose.model('Rating', ratingSchema_);
 
 const createRating = async obj => {
-  const doc = await Rating_.findOne({
-    restaurant: obj.restaurant,
+  const doc = await Rating_.findOne({  //findOne : accès base de données, et await : on attend sa réponse.  doc = document. On regarde si on l'a, le rating
+    restaurant: obj.restaurant, //On cherche en fct du user et du restaurant lié
     user: obj.user
   }).exec();
   if (doc) {
-    doc.rating = obj.rating;
-    doc.save();
+    doc.rating = obj.rating;  //Crée ou update un rating  doc.rating -> bdd , et obj.rating -> js. On prend le rating que la personne vient de mettre et on le met dans la bdd
+    doc.save();  //On envoie la sauvegarde en bdd. On update l'ancien
     return;
   }
-
-  return new Rating_(obj).save();
+  return new Rating_(obj).save(); //On crée un nouveau si ya pas eu d'ancien
 };
 
 const getTheRating = obj =>
   Rating_.findOne(obj)
-    .populate('restaurant')
     .exec();
 
 module.exports = {createRating, getTheRating};
